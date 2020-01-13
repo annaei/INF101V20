3dcbc4a93149271caeac3ff3b7755c9662458f0e
Anna Maria Eilertsen <anna.eilertsen@uib.no>
2020-01-13 19:27:01 +0100

    exercise 00

---
+++ INF101-V20-Ex00/src/main/java/inf101/v19/ex0/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+package inf101.v19.ex0.helloworld;
+
+/**
+ * 
+ * @author anna
+ *
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		String helloWorld = getHelloWorld();
+		System.out.println(helloWorld);
+	}
+
+	public static String getHelloWorld() {
+		return "Hello World!";
+	}
+}
+	
---
+++ INF101-V20-Ex00/src/test/java/inf101/v19/ex0/helloworld/HelloWorldTest.java
@@ -0,0 +1,25 @@
+package inf101.v19.ex0.helloworld;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.Runtime.Version;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest extends HelloWorld {
+
+	@Test 
+	void testGetHelloWorld() {
+		assertTrue(getHelloWorld().equals("Hello World!"));
+	}
+
+	@Test
+	void testJavaVersion() {
+		String minimalJavaVersion = "13.0.1";
+		String currentJavaVersion = System.getProperty("java.version");
+		Version current = Runtime.Version.parse(currentJavaVersion);
+		Version minimal = Runtime.Version.parse(minimalJavaVersion);
+		assertTrue(current.compareTo(minimal) >= 0, "Your Java version is too low, should be at least 13.0.1. Current version is: " + currentJavaVersion);
+	}
+
+}
